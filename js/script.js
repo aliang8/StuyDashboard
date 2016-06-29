205b321fc3109bec4be521b33844cac14ca7e3d8
Brian Yang <brian.yang328@gmail.com>
2016-06-29 10:58:04 -0400

    Fix bugs related to textarea input

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
+function isEmpty(element) {
+    return !$.trim(element.val());
+}
+
 $(document).ready(function(){
-    $('#button').click(function(){
-        var toAdd = $('textarea').val();
-        $('.list').append('<label><input type = "checkbox" value = "' + toAdd + '">' + toAdd + '</label>');
-    });
-	$('textarea').keypress(function(event) {
-		if(event.which == 13){
-			var toAdd = $('textarea').val();
-			$('.list').append('<label><input type = "checkbox" value = "' 
-			+ toAdd + '">' + toAdd + '</label>');
-		}
-	});
+    $('#add-to-list').click(function(){
+	if (!isEmpty($('textarea'))) {
+            var toAdd = $('textarea').val();
+            $('.list').append('<label><input type = "checkbox" value = "' + toAdd + '">' + toAdd + '</label>');
+	}
+    }); 
+    // $('textarea').keypress(function(event) {
+    // 	if(event.which == 13){
+    // 	    var toAdd = $('textarea').val();
+    // 	    $('.list').append('<label><input type = "checkbox" value = "' 
+    // 			      + toAdd + '">' + toAdd + '</label>');
+    // 	}
+    // });
     $(document).on('click','label',function(){
         $(this).remove();
     });
