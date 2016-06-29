d33b66440a47b58bf383dab89de4233ba8faa6ce
aliang8 <aliang8@stuy.edu>
2016-06-29 00:13:08 -0400

    sleepy

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
     $('#button').click(function(){
         var toAdd = $('textarea').val();
-        $('.list').append('<label><input type = "checkbox" value = "' + toAdd + '">' + toAdd + '</label><br>');
+        $('.list').append('<label><input type = "checkbox" value = "' + toAdd + '">' + toAdd + '</label>');
     });
+	$('textarea').keypress(function(event) {
+		if(event.which == 13){
+			var toAdd = $('textarea').val();
+			$('.list').append('<label><input type = "checkbox" value = "' 
+			+ toAdd + '">' + toAdd + '</label>');
+		}
+	});
     $(document).on('click','label',function(){
         $(this).remove();
     });
