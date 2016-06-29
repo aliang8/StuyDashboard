cd27f8ec76e8c9bdae8e3db9802980c12bde60b1
aliang8 <aliang8@stuy.edu>
2016-06-29 13:37:31 -0400

    Combined panel and checklist

--- js/script.js
+++ js/script.js
@@ -10,12 +10,6 @@ $(document).ready(function(){
 			+ toAdd + '">' + toAdd + '</label></div></div>');
 	}
     });
-    $('#add-to-list').click(function(){
-	if (!isEmpty($('textarea'))) {
-            var toAdd = $('textarea').val();
-            $('.list').append('<label><input type = "checkbox" value = "' + toAdd + '">' + toAdd + '</label>');
-	}
-    }); 
     // $('textarea').keypress(function(event) {
     // 	if(event.which == 13){
     // 	    var toAdd = $('textarea').val();
