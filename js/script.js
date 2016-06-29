e0f3613af099384ac969ceda91e22e25952a6151
aliang8 <aliang8@stuy.edu>
2016-06-28 23:29:51 -0400

    idkwhy

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#button').click(function(){
-        var toAdd = $('input[name=checkListItem]').val();
+        var toAdd = $('textarea').val();
         $('.list').append('<label><input type = "checkbox" value = "' + toAdd + '">' + toAdd + '</label><br>');
     });
     $(document).on('click','label',function(){
