d981c120cd346c9c8a23b3d2962f45068b80e495
Brian Yang <brian.yang328@gmail.com>
2016-06-30 23:21:36 -0400

    Fix month and year change on calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,72 +1,71 @@
+var currentMonth;
+
+var getMonthName = function(month) {
+    switch(month){
+    case 0:
+	currentMonth = "January";
+	break;
+    case 1:
+	currentMonth = "February"
+	break;
+    case 2:
+	currentMonth = "March"
+	break;
+    case 3:
+	currentMonth = "April"
+	break;
+    case 4:
+	currentMonth = "May"
+	break;
+    case 5:
+	currentMonth = "June"
+	break;
+    case 6:
+	currentMonth = "July"
+	break;
+    case 7:
+	currentMonth = "August"
+	break;
+    case 8:
+	currentMonth = "September"
+	break;
+    case 9:
+	currentMonth = "October"
+	break;
+    case 10:
+	currentMonth = "November"
+	break;
+    case 11:
+	currentMonth = "December"
+	break;
+    }
+}
+
 $(document).ready(function(){
-		var currentMonth;
-		var d = new Date();
-		var month = d.getMonth();
-			switch(month){
-				case 0:
-					currentMonth = "January";
-					break;
-				case 1:
-					currentMonth = "February"
-					break;
-				case 2:
-					currentMonth = "March"
-					break;
-				case 3:
-					currentMonth = "April"
-					break;
-				case 4:
-					currentMonth = "May"
-					break;
-				case 5:
-					currentMonth = "June"
-					break;
-				case 6:
-					currentMonth = "July"
-					break;
-				case 7:
-					currentMonth = "August"
-					break;
-				case 8:
-					currentMonth = "September"
-					break;
-				case 9:
-					currentMonth = "October"
-					break;
-				case 10:
-					currentMonth = "November"
-					break;
-				case 11:
-					currentMonth = "December"
-					break;
-			}
-		var currentYear = d.getFullYear();
-		$('.m').html(currentMonth);
-		$('.y').html(currentYear);
-		$('.prev').click(function(){
-			month-=1;
-			$('.m').html(currentMonth);
-			/*
-			var year = parseInt($('.y').html());
-			var prev_Year = year-1;
-			if(month == 12){
-			$('.y').html(prev_Year);
-			}
-			*/
-		});
-		$('.next').click(function(){
-			/*
-			var month = parseInt($('.m').html());
-			var next_Month = month+1;
-			if(next_Month > 12){
-				next_Month = 1;
-			}
-			$('.m').html(next_Month);
-			var year = parseInt($('.y').html());
-			var next_Year = year+1;
-			if(next_Month == 1){
-				$('.y').html(next_Year);
-			}
-			*/
-		});
+    var d = new Date();
+    var monthNum = d.getMonth();
+    getMonthName(monthNum);
+    var currentYear = d.getFullYear();
+    $('.m').html(currentMonth);
+    $('.y').html(currentYear);
+    $(document).on("click", ".prev", function(){
+	monthNum -= 1;
+	if (monthNum == -1) {
+	    monthNum = 11;
+	    currentYear -= 1;
+	}
+	getMonthName(monthNum);
+	$('.m').html(currentMonth);
+	$('.y').html(currentYear);
+    });
+    $(document).on("click", ".next", function() {
+	monthNum += 1;
+	if (monthNum == 12) {
+	    monthNum = 0;
+	    currentYear += 1;
+	}
+	getMonthName(monthNum);
+	$('.m').html(currentMonth);
+	$('.y').html(currentYear);
+    });
 });
