c50bca23627cf77aae352416b289a28317f65434
aliang8 <aliang8@stuy.edu>
2016-07-11 22:51:45 -0400

    fixed

--- js/cc.js
+++ js/cc.js
@@ -10,44 +10,53 @@ if (!canvas.getContext) {
 
 if (canvas.getContext) {
     var ctx = canvas.getContext("2d");
-    var xcor = 400-(i-1)*30 +(j-1)*60;
-    var ycor = 25+(i-1)*50;
     for (i = 1; i <= 4; i++){
-	for (j = 1; j <= i; j++){
-      	    var radius = 20;
-      	    ctx.beginPath();
-	    ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
-	    ctx.closePath();
-	    var grd=ctx.createRadialGradient(xcor+10,ycor-10,1,xcor,ycor,20);
-	    grd.addColorStop(0,"white");
-	    grd.addColorStop(1,"red");
-	    ctx.fillStyle = grd;
-	    ctx.fill();
-	    ctx.stroke();
-	}
+		for (j = 1; j <= i; j++){
+			var xcor = 400-(i-1)*30 +(j-1)*60;
+			var ycor = 25+(i-1)*50;
+			var radius = 20;
+			ctx.beginPath();
+			ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
+			ctx.closePath();
+			var grd=ctx.createRadialGradient(xcor+10,ycor-10,1,xcor,ycor,20);
+			grd.addColorStop(0,"white");
+			grd.addColorStop(1,"red");
+			ctx.fillStyle = grd;
+			ctx.fill();
+			ctx.stroke();
+		}
     }
     for (i = 9; i <=13 ; i++){
-	for (j = 1; j <= i; j++){
-	    ctx.beginPath();
-	    ctx.arc(400-(i-1)*30 + (j-1)*60, 25+(i-1)*50,20,0,2*Math.PI,false);
-	    ctx.closePath();
-	    ctx.stroke();
-	}
+		for (j = 1; j <= i; j++){
+			var xcor = 400-(i-1)*30 + (j-1)*60;
+			var ycor = 25+(i-1)*50;
+			var radius = 20;
+			ctx.beginPath();
+			ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
+			ctx.closePath();
+			ctx.stroke();
+		}
     }
     for (i = 13; i >= 10; i--){
-	for (j = 1; j <= i; j++){
-	    ctx.beginPath();
-	    ctx.arc(400-(i-1)*30 + (j-1)*60, 325-(i-11)*50,20,0,2*Math.PI,false);
-	    ctx.closePath();
-	    ctx.stroke();
-	}
+		for (j = 1; j <= i; j++){
+			var xcor = 400-(i-1)*30 + (j-1)*60;
+			var ycor = 325-(i-11)*50;
+			var radius = 20;
+			ctx.beginPath();
+			ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
+			ctx.closePath();
+			ctx.stroke();
+		}
     }
     for (i = 1; i <= 4; i++){
-	for (j = 1; j <= i; j++){
-	    ctx.beginPath();
-	    ctx.arc(400-(i-1)*30 +(j-1)*60, 825-(i-1)*50,20,0,2*Math.PI,false);
-	    ctx.closePath();
-	    ctx.stroke();
-	}
+		for (j = 1; j <= i; j++){
+			var xcor = 400-(i-1)*30 + (j-1)*60;
+			var ycor = 825-(i-1)*50;
+			var radius = 20;
+			ctx.beginPath();
+			ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
+			ctx.closePath();
+			ctx.stroke();
+		}
     }
 }
