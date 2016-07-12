2c0d47506f4824ebe48734d1c4e16da00fcef8ef
Brian Yang <brian.yang328@gmail.com>
2016-07-11 23:32:48 -0400

    Set up framework for animation

--- js/cc.js
+++ js/cc.js
@@ -10,53 +10,81 @@ if (!canvas.getContext) {
 
 if (canvas.getContext) {
     var ctx = canvas.getContext("2d");
-    for (i = 1; i <= 4; i++){
-		for (j = 1; j <= i; j++){
-			var xcor = 400-(i-1)*30 +(j-1)*60;
-			var ycor = 25+(i-1)*50;
-			var radius = 20;
-			ctx.beginPath();
-			ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
-			ctx.closePath();
-			var grd=ctx.createRadialGradient(xcor+10,ycor-10,1,xcor,ycor,20);
-			grd.addColorStop(0,"white");
-			grd.addColorStop(1,"red");
-			ctx.fillStyle = grd;
-			ctx.fill();
-			ctx.stroke();
-		}
+
+    // call setup
+    setup();
+
+    // requestAnimationFrame defined for fallback on various browsers that may not support
+    // window.requestAnimationFrame(callback)
+    var requestAnimationFrame =
+	window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	function(callback) {
+            return setTimeout(callback, 1);
+	};
+
+    /* ============================================
+       ANIMATION
+       ========================================= */
+
+    // runs once
+    function setup() {
+	requestAnimationFrame(draw);
     }
-    for (i = 9; i <=13 ; i++){
-		for (j = 1; j <= i; j++){
-			var xcor = 400-(i-1)*30 + (j-1)*60;
-			var ycor = 25+(i-1)*50;
-			var radius = 20;
-			ctx.beginPath();
-			ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
-			ctx.closePath();
-			ctx.stroke();
-		}
+
+    function draw() {
+	for (i = 1; i <= 4; i++){
+	    for (j = 1; j <= i; j++){
+		var xcor = 400-(i-1)*30 +(j-1)*60;
+		var ycor = 25+(i-1)*50;
+		var radius = 20;
+		ctx.beginPath();
+		ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
+		ctx.closePath();
+		var grd=ctx.createRadialGradient(xcor+10,ycor-10,1,xcor,ycor,20);
+		grd.addColorStop(0,"white");
+		grd.addColorStop(1,"red");
+		ctx.fillStyle = grd;
+		ctx.fill();
+		ctx.stroke();
+	    }
+	}
+	for (i = 9; i <=13 ; i++){
+	    for (j = 1; j <= i; j++){
+		var xcor = 400-(i-1)*30 + (j-1)*60;
+		var ycor = 25+(i-1)*50;
+		var radius = 20;
+		ctx.beginPath();
+		ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
+		ctx.closePath();
+		ctx.stroke();
+	    }
+	}
+	for (i = 13; i >= 10; i--){
+	    for (j = 1; j <= i; j++){
+		var xcor = 400-(i-1)*30 + (j-1)*60;
+		var ycor = 325-(i-11)*50;
+		var radius = 20;
+		ctx.beginPath();
+		ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
+		ctx.closePath();
+		ctx.stroke();
+	    }
+	}
+	for (i = 1; i <= 4; i++){
+	    for (j = 1; j <= i; j++){
+		var xcor = 400-(i-1)*30 + (j-1)*60;
+		var ycor = 825-(i-1)*50;
+		var radius = 20;
+		ctx.beginPath();
+		ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
+		ctx.closePath();
+		ctx.stroke();
+	    }
+	}
     }
-    for (i = 13; i >= 10; i--){
-		for (j = 1; j <= i; j++){
-			var xcor = 400-(i-1)*30 + (j-1)*60;
-			var ycor = 325-(i-11)*50;
-			var radius = 20;
-			ctx.beginPath();
-			ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
-			ctx.closePath();
-			ctx.stroke();
-		}
-    }
-    for (i = 1; i <= 4; i++){
-		for (j = 1; j <= i; j++){
-			var xcor = 400-(i-1)*30 + (j-1)*60;
-			var ycor = 825-(i-1)*50;
-			var radius = 20;
-			ctx.beginPath();
-			ctx.arc(xcor,ycor,radius,0,2*Math.PI,false);
-			ctx.closePath();
-			ctx.stroke();
-		}
-    }
+
 }
